ce391606562c5106b0012e682cc0e55abc8083a3
EspinelThomas03 <tespinel03@gmail.com>
2024-09-20 00:47:45 -0500

    commit 4

--- To Do List/src/co/edu/uptc/view/subPanels/MediumSubPanel.java
+++ To Do List/src/co/edu/uptc/view/subPanels/MediumSubPanel.java
@@ -21,7 +21,7 @@ import co.edu.uptc.view.panels.LeftPanel;
 public class MediumSubPanel extends JPanel{
 
     public MediumSubPanel(){
-        setLayout(null);
+        setLayout(new BorderLayout());
         setPreferredSize(new Dimension(LeftPanel.WIDTH, 550));
         setBackground(Color.WHITE);
         intitComponents();
@@ -29,20 +29,21 @@ public class MediumSubPanel extends JPanel{
 
 
     private void intitComponents(){
-        addLineSeparator();
-        addButtons();
+        addButtonMyDay();
         addButtonImportant();
         addButtonFinished();
+        addButtonPlaned();
+        addLineSeparator();
     }
 
     private void addLineSeparator(){
         JPanel blackLine = new JPanel();
         blackLine.setBackground(Color.BLACK);
-        blackLine.setPreferredSize(new Dimension(WIDTH, 2));
+        blackLine.setPreferredSize(new Dimension(LeftPanel.WIDTH, 2));
         add(blackLine, BorderLayout.SOUTH);
     }
 
-    private void addButtons(){
+    private void addButtonMyDay(){
         try {
             BufferedImage iconSun = ImageIO.read(new File("To Do List\\src\\resources\\SunIcon.png"));
             Image scaledImg = iconSun.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
@@ -56,9 +57,9 @@ public class MediumSubPanel extends JPanel{
             buttonMyDay.setFocusPainted(false);
             buttonMyDay.setIcon(iconFinalSun);
             // buttonMyDay.setHorizontalAlignment(SwingConstants.RIGHT);
-            buttonMyDay.setBounds(0,0,WIDTH,50);
-            buttonMyDay.setPreferredSize(new Dimension(WIDTH, 50));
-            add(buttonMyDay);
+            // buttonMyDay.setBounds(0,0,WIDTH,50);
+            // buttonMyDay.setPreferredSize(new Dimension(WIDTH, 50));
+            add(buttonMyDay,BorderLayout.NORTH);
         } catch (Exception e) {
             e.getMessage();
         }
@@ -78,9 +79,9 @@ public class MediumSubPanel extends JPanel{
             buttonImportants.setFocusPainted(false);
             buttonImportants.setIcon(iconFinalStar);
             // buttonImportants.setHorizontalAlignment(SwingConstants.RIGHT);
-            buttonImportants.setBounds(0,50,WIDTH,50);
-            buttonImportants.setPreferredSize(new Dimension(WIDTH, 50));
-            add(buttonImportants);
+            // buttonImportants.setBounds(0,50,WIDTH,50);
+            // buttonImportants.setPreferredSize(new Dimension(WIDTH, 50));
+            add(buttonImportants, BorderLayout.CENTER);
         } catch (Exception e) {
             e.getMessage();
         }
@@ -102,7 +103,29 @@ public class MediumSubPanel extends JPanel{
             // buttonImportants.setHorizontalAlignment(SwingConstants.RIGHT);
             buttonWorksReady.setBounds(0,100,WIDTH,50);
             buttonWorksReady.setPreferredSize(new Dimension(WIDTH, 50));
-            add(buttonWorksReady);
+            add(buttonWorksReady,BorderLayout.CENTER);
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    private void addButtonPlaned(){
+        try {
+            BufferedImage iconCalendar = ImageIO.read(new File("To Do List\\src\\resources\\calendaryIcon.png"));
+            Image scaledImg = iconCalendar.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+            ImageIcon iconFinalCalendar = new ImageIcon(scaledImg);
+
+            JButton buttonWorksCalendar = new JButton("Planeadas");
+            buttonWorksCalendar.setFont(new Font(ConstanFonts.font, ALLBITS, ConstanSize.size));
+            buttonWorksCalendar.setBackground(this.getBackground());
+            buttonWorksCalendar.setForeground(Color.LIGHT_GRAY);
+            buttonWorksCalendar.setBorderPainted(false);
+            buttonWorksCalendar.setFocusPainted(false);
+            buttonWorksCalendar.setIcon(iconFinalCalendar);
+            // buttonImportants.setHorizontalAlignment(SwingConstants.RIGHT);
+            buttonWorksCalendar.setBounds(0,100,WIDTH,50);
+            buttonWorksCalendar.setPreferredSize(new Dimension(WIDTH, 50));
+            add(buttonWorksCalendar,BorderLayout.SOUTH);
         } catch (Exception e) {
             e.getMessage();
         }
