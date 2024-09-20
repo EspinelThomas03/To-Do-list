bcf3e6922399581757fe6acd89ab32a8b9d5aa80
EspinelThomas03 <tespinel03@gmail.com>
2024-09-20 15:02:42 -0500

    Commit actualización clase 20 de septiembre

---
+++ To Do List/src/co/edu/uptc/view/constans/ConstantsColors.java
@@ -0,0 +1,8 @@
+package co.edu.uptc.view.constans;
+
+import java.awt.Color;
+
+public class ConstantsColors {
+    public static final Color colorForeground = new Color(126, 125, 126);
+    public static final Color colorBackgroungChanged = new Color(200, 180, 155);
+}
